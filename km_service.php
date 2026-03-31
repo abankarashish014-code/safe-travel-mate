f17d71a8744c6805cf543a18f8ef189ffffab253
abankarashish014-code <abankarashish014@gmail.com>
2026-03-31 21:19:50 +0530

    Add files via upload

---
+++ km_service.php
@@ -0,0 +1,6 @@
+<form action="calculate_km.php" method="POST">
+    Name: <input type="text" name="name"><br>
+    Distance (km): <input type="number" name="distance"><br>
+    Rate per km: <input type="number" name="rate"><br>
+    <input type="submit" value="Calculate">
+</form>
